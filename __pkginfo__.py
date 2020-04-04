dfaa47d26e86fb026aa8882732aec2195c5510e1
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-04 20:44:07 +0100

    updated __pkginfo__.py to latest version and moved .desktop file creation into setup.py

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,28 +1,12 @@
-#  Copyright (C) 2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 
-#  This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+import pathlib
 
-import os.path
-import sys
-
-from indicator_syncthing import VERSION, APPINDICATOR_ID
+from indicator_syncthing import APPINDICATOR_ID
 
 modname = APPINDICATOR_ID
 py_modules = ["indicator_syncthing"]
-entry_points       = {
+entry_points = {
 	'console_scripts': [
 		f'{APPINDICATOR_ID}=indicator_syncthing:main',
 			]}
@@ -50,33 +34,16 @@ github_username = "domdfcoding"
 web = github_url = f"https://github.com/{github_username}/{modname}"
 
 
-def get_srcdir():
-	filename = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
-	return os.path.realpath(filename)
-
-
-def read(*rnames):
-	return open(os.path.join(get_srcdir(), *rnames)).read()
-
-
-install_requires = [read("requirements.txt")]
-
-
 # Get info from files; set: long_description
-long_description = (read("README.rst") + '\n')
+if pathlib.Path.cwd().name == "doc-source":
+	print(pathlib.Path.cwd().parent / "README.rst")
+	install_requires = (pathlib.Path.cwd().parent / "requirements.txt").read_text().split("\n")
+	long_description = (pathlib.Path.cwd().parent / "README.rst").read_text() + '\n'
+else:
+	print(pathlib.Path("README.rst"))
+	install_requires = pathlib.Path("requirements.txt").read_text().split("\n")
+	long_description = pathlib.Path("README.rst").read_text() + '\n'
 
-# Create .desktop file
-with open(f"{APPINDICATOR_ID}.desktop", "w") as desktop:
-	desktop.write(f"""[Desktop Entry]
-Version={VERSION}
-Name={modname}
-Comment={short_desc}
-Exec={APPINDICATOR_ID}
-Icon=syncthing
-Terminal=false
-Type=Application
-Categories=Utility;
-""")
 
 data_files = [
 		('share/applications', [f'{APPINDICATOR_ID}.desktop']),
--- setup.py
+++ setup.py
@@ -5,10 +5,27 @@ from __pkginfo__ import (
 	author, author_email, install_requires,
 	license, long_description, classifiers,
 	entry_points, modname, py_modules,
-	short_desc, VERSION, web, data_files
+	short_desc, web, data_files,
 	)
 
-from setuptools import setup, find_packages
+from indicator_syncthing import VERSION, APPINDICATOR_ID
+
+
+from setuptools import setup
+
+
+# Create .desktop file
+with open(f"{APPINDICATOR_ID}.desktop", "w") as desktop:
+	desktop.write(f"""[Desktop Entry]
+Version={VERSION}
+Name={modname}
+Comment={short_desc}
+Exec={APPINDICATOR_ID}
+Icon=syncthing
+Terminal=false
+Type=Application
+Categories=Utility;
+""")
 
 
 setup(
